b092259b2825ca8178c36d3322a07d86b56c4548
ngianguyen81 <ngianguyen81@gmail.com>
2022-03-11 15:36:00 +0700

    cleanup; add route for notesView

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ void main() async {
     routes: {
       '/login/': (context) => const LoginView(),
       '/register/': (context) => const RegisterView(),
+      '/notes/': (context) => const NotesView(),
     },
   ));
 }
